4b982a69a1a37adcc3722e55d7d74b6f33b7f693
Jeffrey Lemoine <jeffmlife@gmail.com>
2020-04-08 19:42:03 -0700

    Best solution
    This is what I consider to be the answer. It's taken from https://leetcode.com/problems/permutations/discuss/18241/One-Liners-in-Python 
    
    My solution failed for reasons i will describe in code comments soon.

--- Problems/Medium/permutations-solution.py
+++ Problems/Medium/permutations-solution.py
@@ -1,23 +1,7 @@
 class Solution:
     def _permute(self, nums):
-        if len(nums) > 2: 
-            tmp_lol = []
-            loop_nums = list(nums)
-            for i, _ in enumerate(loop_nums):
-                tmp_nums = [nums[i]] + nums[:i] + nums[i + 1:] 
-                tmp_lol.append(tmp_nums)
-                sub_perms = Solution._permute(self, nums[:i] + nums[i + 1:])
-                tmp_nums_r = [nums[i], *sub_perms]
-
-                tmp_lol.append(tmp_nums_r)
-            return tmp_lol
-        
-        elif len(nums) == 2:
-            return [nums[1], nums[0]]
-        elif len(nums) == 1: 
-            return nums
-        else:
-            raise Exception("Major error")
+        return [[n] + p for i,n in enumerate(nums)
+        for p in _permute(nums[:i] or nums[i+1:])] + [[]]
     
     
     def permute(self, nums: List[int]) -> List[List[int]]:
