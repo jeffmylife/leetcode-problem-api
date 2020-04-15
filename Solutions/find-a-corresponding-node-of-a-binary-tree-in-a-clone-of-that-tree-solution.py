4e14d17302f646c6a7a5acf7ab4cd6c7d421d5e7
jeffmylife <jeffmlife@gmail.com>
2020-04-14 18:50:25 -0700

    lol it's actually dfs

--- Solutions/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree-solution.py
+++ Solutions/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree-solution.py
@@ -6,22 +6,21 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def _bfs(self, node, target):
+    def _dfs(self, node, target):
         if node: 
             if node.val==target.val:
                 return node
-            return Solution._bfs(self, node.left, target) or Solution._bfs(self, node.right, target)
+            return Solution._dfs(self, node.left, target) or Solution._dfs(self, node.right, target)
             
     
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        return Solution._bfs(self, cloned, target)
+        return Solution._dfs(self, cloned, target)
 
 """
 This works simply enough. Recursively call on node: if None return None, if it's target return itself, otherwise recursivley call on left and right. That return statement works because of the "or" which conveniently returns the first non-None operand or None if they're both None. 
 
 --Problems--
 Unfortunely for this solution, it runs on all nodes of the tree. Also, it cannot handle duplicate values. 
-
 
 """
 
